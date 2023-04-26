6ea018045bb586378523c74920c9608d8ce26e4b
賴廷凱 <fatelai1017@gmail.com>
2023-04-26 16:38:54 +0800

    update other tree method

--- HW3/GrpcClient/GrpcClient/Program.cs
+++ HW3/GrpcClient/GrpcClient/Program.cs
@@ -1,12 +1,85 @@
 ﻿using System.Threading.Tasks;
+using Grpc.Core;
 using Grpc.Net.Client;
 using GrpcService;
 
-using var channel = GrpcChannel.ForAddress("https://localhost:7213");
-var client = new Greeter.GreeterClient(channel);
-var reply = await client.SayHelloAsync(
-                  new HelloRequest { Name = "GreeterClient" });
+GrpcChannel channel = null;
+Greeter.GreeterClient client = null;
+initGrpc();
 
-Console.Write(reply);
+//await GrpcBaseAsync();
+//await GrpcServerSideAsync();
+//await GrpcClientSideAsync();
+await GrpcServerClientAsync();
 
 Console.Read();
+
+void initGrpc()
+{
+    channel = GrpcChannel.ForAddress("https://localhost:7213");
+    client = new Greeter.GreeterClient(channel);
+}
+
+async Task GrpcBaseAsync()
+{
+    var reply = await client.SayHelloAsync(
+                      new HelloRequest { Name = "GreeterClient" });
+
+    Console.Write(reply);
+}
+
+async Task GrpcClientSideAsync()
+{
+    var call = client.SayHelloByStream();
+
+    List<HelloRequest> ServerStreamData = new List<HelloRequest>()
+    {
+        new HelloRequest { Name = "GreeterClient1" },
+        new HelloRequest { Name = "GreeterClient2" },
+        new HelloRequest { Name = "GreeterClient3" },
+        new HelloRequest { Name = "GreeterClient4" }
+    };
+
+    foreach (var request in ServerStreamData)
+    {
+        await call.RequestStream.WriteAsync(request);
+    }
+
+    // Complete the stream
+    await call.RequestStream.CompleteAsync();
+
+    Console.Write(await call.ResponseAsync);
+}
+
+async Task GrpcServerSideAsync()
+{
+    var responseStream = client.GetHelloStream(new HelloRequest() {  Name = "test" });
+
+    await foreach (var response in responseStream.ResponseStream.ReadAllAsync())
+    {
+        Console.WriteLine(response.Message);
+    }
+}
+
+async Task GrpcServerClientAsync()
+{
+    List<HelloRequest> ServerStreamData = new List<HelloRequest>()
+    {
+        new HelloRequest { Name = "GreeterClient1" },
+        new HelloRequest { Name = "GreeterClient2" },
+        new HelloRequest { Name = "GreeterClient3" },
+        new HelloRequest { Name = "GreeterClient4" }
+    };
+
+    var call = client.HelloStream();
+
+    foreach (var request in ServerStreamData)
+    {
+        await call.RequestStream.WriteAsync(request);
+    }
+
+    await foreach (var response in call.ResponseStream.ReadAllAsync())
+    {
+        Console.WriteLine(response.Message);
+    }
+}
--- HW3/GrpcService/GrpcService/Services/GreeterService.cs
+++ HW3/GrpcService/GrpcService/Services/GreeterService.cs
@@ -1,5 +1,6 @@
 using Grpc.Core;
 using GrpcService;
+using System.Text.Json;
 
 namespace GrpcService.Services
 {
@@ -18,5 +19,44 @@ namespace GrpcService.Services
                 Message = "Hello " + request.Name
             });
         }
+
+        //Client-side
+        public override async Task<HelloReply> SayHelloByStream(IAsyncStreamReader<HelloRequest> requestStream, ServerCallContext context)
+        {
+            List<string> responseData = new();
+
+            while (await requestStream.MoveNext()) {
+              HelloRequest request = requestStream.Current;
+
+                responseData.Add(request.Name);
+
+                Console.WriteLine(request.Name);
+            }
+
+            return new HelloReply()
+            {
+                Message =
+                JsonSerializer.Serialize(responseData)
+            };
+        }
+        public override async Task GetHelloStream(HelloRequest request, IServerStreamWriter<HelloReply> responseStream, ServerCallContext context)
+        {
+            await responseStream.WriteAsync(new HelloReply() { Message = request.Name + "1"});
+            await responseStream.WriteAsync(new HelloReply() { Message = request.Name + "2" });
+            await responseStream.WriteAsync(new HelloReply() { Message = request.Name + "3" });
+        }
+
+        public override async Task HelloStream(IAsyncStreamReader<HelloRequest> requestStream, IServerStreamWriter<HelloReply> responseStream, ServerCallContext context)
+        {
+            while (await requestStream.MoveNext())
+            {
+                HelloRequest request = requestStream.Current;
+
+                await responseStream.WriteAsync(new HelloReply() { Message = request.Name + "1" });
+
+                Console.WriteLine(request.Name);
+            }
+        }
+
     }
 }
